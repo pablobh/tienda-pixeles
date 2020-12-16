ed87cd11053ed8c8dc92afa99a617cde02de35cc
Pablo Bustamante <pablo@4topixel.com>
2020-12-15 22:08:38 -0500

    Botón para modificar las cantidades a agregar al carrito

--- src/components/global/ItemCard.jsx
+++ src/components/global/ItemCard.jsx
@@ -1,4 +1,13 @@
-function ItemCard(props) {
+import { useState } from 'react';
+
+const ItemCard = (props) => {
+    const [cantidad, cambiarCantidad] = useState(0);
+
+    function quitarCantidad() {
+        if (cantidad) {
+            cambiarCantidad(cantidad - 1);
+        }
+    }
     return (
         <div className="column">
             <div className="card has-background-white has-shadow" style={{height: '100%', radius: '3px' }}>
@@ -15,9 +24,22 @@ function ItemCard(props) {
                             <span className="is-size-6 regular-price-product">
                                 ${props.precioProducto}
                             </span>
-                        </p>                
-                        <a className="button has-background-naranja has-text-white" href="!#">Agregar al carrito</a>
-                        <a className="button has-background-white has-text-secondary ml-4" href="!#">Ver detalles</a>
+                        </p>
+                        <div className="field has-addons has-addons-centered">
+                            <div className="control">
+                                <button className="button is-danger is-light" disabled={!cantidad ? 'disabled' : null } onClick={quitarCantidad}>-</button>
+                            </div>
+                            <div className="control">
+                                <input className="input is-light has-text-centered" type="text" value={cantidad} readOnly/>
+                            </div>
+                            <div className="control">
+                                <button className="button is-success is-light" onClick={() => cambiarCantidad(cantidad + 1)}>+</button>
+                            </div>
+                        </div>
+                        <div className="field is-grouped is-grouped-centered">
+                            <a className="button is-info mr-2" href="!#">Ver detalles</a>
+                            <a className="button is-primary" href="!#">Agregar al carrito</a>
+                        </div>
                     </div>
                 </div>
             </div>
