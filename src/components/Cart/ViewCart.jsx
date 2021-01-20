82db70ce25f8b164724f9a068e2b1e6175976444
Pablo Bustamante <pablo@4topixel.com>
2021-01-20 10:44:30 -0500

    Condicional en el carrito cuando está vacío

--- src/components/Cart/ViewCart.jsx
+++ src/components/Cart/ViewCart.jsx
@@ -12,55 +12,64 @@ const ViewCart = () => {
         <section className="section is-medium" id="cart">
             <div className="container">
                 <div className="cart">
-                    <div className="columns is-pulled-right">
-                        <div className="column is-2">
-                            <Link to="/" className="button is-outlined is-primary">Regresar</Link>
-                        </div>
-                    </div>
+                    {
+                        data.items.length !== 0 ?
+                            <>
+                                <div className="columns is-pulled-right">
+                                    <div className="column is-2">
+                                        <Link to="/" className="button is-outlined is-primary">Regresar</Link>
+                                    </div>
+                                </div>
+                                <div className="columns">
+                                    <div className="column is-4">
+                                        <h1 className="title is-2 has-text-morado">Tu carrito</h1>
+                                    </div>
+                                </div>
 
-                    <div className="columns">
-                        <div className="column is-4">
-                            <h1 className="title is-2 has-text-morado">Tu carrito</h1>
-                        </div>
-                    </div>
-                    
-                    <div className="columns">
-                        <div className="column">
-                            <table className="table is-striped is-hoverable is-fullwidth">
-                                <thead>
-                                    <tr className="has-background-space-black">
-                                        <th className="has-text-cremita">Categoría</th>
-                                        <th className="has-text-cremita">Producto</th>
-                                        <th className="has-text-cremita">Cantidad</th>
-                                        <th className="has-text-cremita">Unidad</th>
-                                        <th className="has-text-cremita">Total</th>
-                                        <th className="has-text-cremita is-narrow">Quitar</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {
-                                        data.items.map(item =>
-                                            <CartItem
-                                                categoriaBonitaProducto = {item.item.categoriaBonita}
-                                                nombreProducto = {item.item.nombre}
-                                                cantidadProducto = {item.cantidad}
-                                                precioProducto = {Intl.NumberFormat('es-CO', {style: 'currency', currency: 'COP', minimumFractionDigits: 0}).format(item.item.precio)}
-                                                precioProductoTotal = {Intl.NumberFormat('es-CO', {style: 'currency', currency: 'COP', minimumFractionDigits: 0}).format(item.item.precio * item.cantidad)}>
-                                            </CartItem>
-                                        )
-                                    }
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
+                                <div className="columns">
+                                    <div className="column">
+                                        <table className="table is-striped is-hoverable is-fullwidth">
+                                            <thead>
+                                                <tr className="has-background-space-black">
+                                                    <th className="has-text-cremita">Categoría</th>
+                                                    <th className="has-text-cremita">Producto</th>
+                                                    <th className="has-text-cremita">Cantidad</th>
+                                                    <th className="has-text-cremita">Unidad</th>
+                                                    <th className="has-text-cremita">Total</th>
+                                                    <th className="has-text-cremita is-narrow">Quitar</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                    {
+                                                        data.items.map(item =>
+                                                            <CartItem
+                                                                categoriaBonitaProducto = {item.item.categoriaBonita}
+                                                                nombreProducto = {item.item.nombre}
+                                                                cantidadProducto = {item.cantidad}
+                                                                precioProducto = {Intl.NumberFormat('es-CO', {style: 'currency', currency: 'COP', minimumFractionDigits: 0}).format(item.item.precio)}
+                                                                precioProductoTotal = {Intl.NumberFormat('es-CO', {style: 'currency', currency: 'COP', minimumFractionDigits: 0}).format(item.item.precio * item.cantidad)}>
+                                                            </CartItem>
+                                                        )
+                                                    }
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
 
-                    <div className="columns has-background-space-black is-pulled-right">
-                        <div className="column is-full">
-                            <h3 className="title is-4 has-text-cremita">
-                            Total: {Intl.NumberFormat('es-CO', {style: 'currency', currency: 'COP', minimumFractionDigits: 0}).format(valorTotal)}</h3>
-                        </div>
-                    </div>
-
+                                <div className="columns has-background-space-black is-pulled-right">
+                                    <div className="column is-full">
+                                        <h3 className="title is-4 has-text-cremita">
+                                        Total: {Intl.NumberFormat('es-CO', {style: 'currency', currency: 'COP', minimumFractionDigits: 0}).format(valorTotal)}</h3>
+                                    </div>
+                                </div>
+                            </> :
+                            <div className="columns">
+                                <div className="column is-6 is-offset-4">
+                                    <h1 className="title is-2 has-text-morado">Tu carrito está vacío</h1>
+                                    <Link to="/" className="button is-primary has-text-weight-bold">Agregar Productos</Link>
+                                </div>
+                            </div>
+                        }
                 </div>
             </div>
         </section>
